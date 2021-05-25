bc4d8e1ab7c26935a439432f36804ca1cc33a91b
yuvaltoker <yuvalt2710@gmail.com>
2021-05-25 17:01:57 +0300

    trying running demo1, still fail

--- emane-tutorial/1/start-demo.sh
+++ emane-tutorial/1/start-demo.sh
@@ -1,12 +1,27 @@
-mkdir -p persist/$NODE_NO/var/log
-mkdir -p persist/$NODE_NO/var/run
+demo_path="/emane-tutorial/1"
 
+mkdir -p $demo_path/persist/$NODE_NO/var/log
+mkdir -p $demo_path/persist/$NODE_NO/var/run
+
+echo "im here"
 # start emane
-emane platform$NODE_NO.xml -r -d -l 3 -f persist/$NODE_NO/var/log/emane.log
+emane $demo_path/platform$NODE_NO.xml -r -d -l 3 -f $demo_path/persist/$NODE_NO/var/log/emane.log
 
 # start event daemon
-emaneeventd eventdaemon$NODE_NO.xml -r -d -l 4 -f persist/$NODE_NO/var/log/emaneeventd.log
+emaneeventd $demo_path/eventdaemon$NODE_NO.xml -r -d -l 4 -f p$demo_path/ersist/$NODE_NO/var/log/emaneeventd.log
 
 sleep 1
 # start gpsd
-gpsd -G -n -b $(cat persist/$NODE_NO/var/run/gps.pty)
+gpsd -P $demo_path/persist/$NODE_NO/var/run/gpsd.pid -G -n -b $(cat $demo_path/persist/$NODE_NO/var/run/gps.pty)
+
+#start mgen
+starttime=
+startoption=""
+if [ -n "$starttime" ]; then
+    startoption="start $(date --date "$starttime" "+%H:%M:%S" --utc)GMT"
+fi
+
+mgen input $demo_path/mgen$NODE_NO \
+     output $demo_path/persist/$NODE_NO/var/log/mgen.out  \
+     $startoption
+     txlog &> $demo_path/persist/$NODE_NO/var/log/mgen.log &
