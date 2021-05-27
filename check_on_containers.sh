6395674049540e74bbcab57e684ef316c0e988de
yuvaltoker <yuvalt2710@gmail.com>
2021-05-27 15:44:21 +0300

    working on demo2

--- check_on_containers.sh
+++ check_on_containers.sh
@@ -1,1 +1,10 @@
-for i in $(seq 1 10); do docker exec -it emane-service$i emanesh localhost get table $i nems phy  PathlossEventInfoTable; done
+container_no="2"
+emanesh_command="set config "$container_no" mac delay=.92"
+
+
+
+if [ "$container_no" = "" ]; then 
+    for i in $(seq 1 10); do docker exec -it emane-service$i emanesh localhost $emanesh_command; done
+else
+    docker exec -it emane-service$container_no emanesh localhost $emanesh_command
+fi
